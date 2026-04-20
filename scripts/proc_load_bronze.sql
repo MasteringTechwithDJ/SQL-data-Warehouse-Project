b865b4a1379be34476a0e423ff6ead31ff4d0a0e
Durant27 <77516918+Durant27@users.noreply.github.com>
2026-04-20 17:07:02 -0500

    Create stored procedure to load data into bronze schema
    This stored procedure loads data into the 'bronze' schema from external CSV files, truncating tables before loading.

---
+++ scripts/proc_load_bronze.sql
@@ -0,0 +1,30 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from CSV files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+
+TRUNCATE TABLE bronze.crm_cust_info
+
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\Jasmi\Downloads\cust_info.csv'
+WITH ( 
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+
+);
+
+SELECT COUNT (*)  FROM bronze.crm_cust_info 
